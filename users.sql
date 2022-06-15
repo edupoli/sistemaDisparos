9bf5bb2bbd747059415e3f87890234d55db92820
edupoli <eduardopolicarpo@gmail.com>
2022-06-15 12:35:05 -0300

    add sql insert

---
+++ users.sql
@@ -0,0 +1,2 @@
+INSERT INTO Users VALUES('1', 'Administrador do Sistema ', 'edu.policarpo@gmail.com', 'admin ', '$2a$10$NvFy6RRwAowdFfT8YgGEr.PHxuEHMLAmu9c3knmSkTSJuZWEy7VJ2', 'administrador', 'Administrador ', 'IMG_20220205_134218.jpeg', '2022-03-28 12:03:25', '2022-06-13 15:55:31'
+)
